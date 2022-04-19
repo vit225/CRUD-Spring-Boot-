6ffde1408d141a160f16510935c3b75fa98bd165
Виталий <vitaly@mail.ru>
2022-04-20 07:42:35 +1000

    commit

--- src/main/java/application/services/UserServiceImpl.java
+++ src/main/java/application/services/UserServiceImpl.java
@@ -1,56 +1,38 @@
 package application.services;
 
+import application.dao.UserDao;
 import application.models.User;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @PersistenceContext()
-    private EntityManager entityManager;
+    private UserDao userDao;
 
-
-    @Transactional
     @Override
     public void saveUser(User user) {
-        entityManager.persist(user);
-        entityManager.close();
+        userDao.saveUser(user);
     }
 
-
-    @Transactional
     @Override
     public List<User> findAll() {
-        return entityManager.createQuery("SELECT user FROM User user", User.class).getResultList();
+        return userDao.findAll();
     }
 
-
-    @Transactional
     @Override
     public void deleteById(long id) {
-        User user = entityManager.find(User.class, id);
-        if (user != null) {
-            entityManager.remove(user);
+       userDao.deleteById(id);
         }
-    }
 
-
-    @Transactional
     @Override
     public User findById(long id) {
-        return entityManager.find(User.class, id);
+        return userDao.findById(id);
     }
 
-
-    @Transactional
     @Override
     public void updateUser(User user) {
-        entityManager.merge(user);
+        userDao.updateUser(user);
     }
 }
 
